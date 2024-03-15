487c8ef1ff5df14ae107aa2d9a0ca68e218ce1ac
HUHHWANWOOK <maga_h@naver.com>
2024-03-15 11:36:24 +0900

    message

--- hello.c
+++ hello.c
@@ -5,5 +5,9 @@ int main()
 	printf("hello world\n");
 	printf("git is difficult\n");
 	printf("readd line");
+	printf("branch check");
+	printf("branch2 check");
+	print("branch3 check");
+	print("branch4 check");
 	return 0;
 }
